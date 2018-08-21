b1f2039ae2d03f125a3b93e068cee0959126d2f7
Daniel Max <danielmaxsiegel@gmail.com>
2018-08-21 14:12:58 -0400

    if the visited cookie is set, you get NotOkay. Otherwise you get Okay.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Cookies from 'universal-cookie';
 
 class App extends Component {
   render() {
-    // const cookies = new Cookies();
-    // cookies.set('myCat', 'Pacman', { path: '/' });
-    // console.log(cookies.get('myCat')); // Pacman
-
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Okay Pill</h1>
-        </header>
-        <p className="App-intro">
-          Coming soon.
-        </p>
+        { this.props.MainPage }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Okay from './Okay';
+import NotOkay from './NotOkay';
 import registerServiceWorker from './registerServiceWorker';
+import Cookies from 'universal-cookie';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cookies = new Cookies();
+// cookies.set('visited', 'true', { path: '/' });
+
+function MainPage(props) {
+  const hasVisited = cookies.get('visited');
+  if (hasVisited) {
+    return <NotOkay />;
+  }
+  return <Okay />;
+}
+
+ReactDOM.render(<App MainPage={MainPage()}/>, document.getElementById('root'));
 registerServiceWorker();
